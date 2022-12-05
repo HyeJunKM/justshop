2d4b1b8d2c5948055854cf0285f153abe2a258a0
HyeJunKM <hjkim0122@daum.net>
2022-12-05 14:04:14 +0900

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import WrapComp from './components/WrapComp';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <WrapComp/>
-);
-
